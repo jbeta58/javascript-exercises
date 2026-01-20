c6fc9439d25a9d4f2ff4759ac7a88049968da525
Sebastian <32719807+jbeta58@users.noreply.github.com>
2026-01-20 15:00:30 -0500

    feat: add function to solve exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
 const removeFromArray = function() {
+    let arg = Array.from(arguments);
+    let result = arg[0];
+    for(let i = 1; i < arg.length; i++) {
+        result = result.filter(item => item !== arg[i])
+    }
+    return(result);
 };
-
+    
 // Do not edit below this line
 module.exports = removeFromArray;
