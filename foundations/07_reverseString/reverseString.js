c6364dc394085a22475a2a48b92cf9721d3b4505
Sebastian <32719807+jbeta58@users.noreply.github.com>
2026-01-20 13:11:39 -0500

    feat: create function reverseString to solve exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let answer = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        answer += string.at(i);
+    }
+    return answer;
 };
 
 // Do not edit below this line
