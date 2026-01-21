5f7cda8ee90e847132be4507df74d3a985e5b593
Sebastian <32719807+jbeta58@users.noreply.github.com>
2026-01-20 21:54:34 -0500

    feat: create function to solve exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || 
+        !Number.isInteger(max) ||
+        min < 0 || max < 0) {
+        return "ERROR"
+    }
+    
+    if (min > max) {
+        let temp = min;
+        min = max;
+        max = temp;
+    }
+    
+    let result = 0;
+    let i = min;
+    while (i <= max) {
+        result += i;
+        i++;
+    }
+    return result;
+}
+    
 // Do not edit below this line
 module.exports = sumAll;
