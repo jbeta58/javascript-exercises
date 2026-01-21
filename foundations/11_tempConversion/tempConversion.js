5453ff8ca46a7b9f7534fdd24acbbba7caa67528
Sebastian <32719807+jbeta58@users.noreply.github.com>
2026-01-20 22:17:53 -0500

    feat: create function to solve exercise 11 temp conversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fNumber) {
+  return Math.round(((fNumber - 32) * 5 / 9) * 10 )/ 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cNumber) {
+  return Math.round(((cNumber * 9 / 5) + 32) * 10 )/ 10;
 };
 
 // Do not edit below this line
