bb1a93b8f26f73d32917db33fd20abbc0c2b9768
Sebastian <32719807+jbeta58@users.noreply.github.com>
2026-02-14 08:59:43 -0500

    fix: create calculator formulas

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a -b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((sum, item) => sum + item, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((mult, item) => mult * item, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let mult = 1
+  for (let i = 1; i <= number; i++) {
+    mult *= i;
+  }
+  return mult;
 };
 
 // Do not edit below this line
